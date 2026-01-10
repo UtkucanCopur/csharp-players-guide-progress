ae41c2329ae225ebd1aa2ff5ac2bfffd12dd788f
Utkucan Çopur <utkucan.copur00@gmail.com>
2026-01-10 22:35:43 +0300

    TheCSharpTypeSystem Done
    i completed two challenges in this session

---
+++ Challenges/Part1-TheBasics/TheCSharpTypeSystem/TheVariableShop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheVariableShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 1;
+            short b = 2;
+            int c = 3;
+            long d = 4;
+            sbyte e = 5;
+            ushort f = 6;
+            uint g = 7;
+            ulong h = 8;
+            float i = 9.2f;
+            double j = 10.5;
+            decimal k = 15.4m;
+            char l = 'a';
+            string m = "word";
+            bool n = true;
+            Console.WriteLine($"Variables: {a} {b} {c} {d} {e} {f} {g} {h} {i} {j} {k} {l} {m} {n}");
+        }
+    }
+}
---
+++ Challenges/Part1-TheBasics/TheCSharpTypeSystem/TheVariableShopReturns/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheVariableShopReturns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 200;          
+            short b = -12000;     
+            int c = 150000;       
+            long d = 9_000_000_000L; 
+
+            sbyte e = -100;       
+            ushort f = 60000;     
+            uint g = 3_000_000_000; 
+            ulong h = 18_000_000_000_000_000_000UL; 
+
+            float i = 3.14f;      
+            double j = 12345.6789; 
+            decimal k = 9999.99m; 
+
+            char l = 'Z';         
+            string m = "CSharp";  
+            bool n = false;
+
+            Console.WriteLine($"a={a}, b={b}, c={c}, d={d}, e={e}, f={f}, g={g}, h={h}, i={i}, j={j}, k={k}, l={l}, m={m}, n={n}");
+
+
+        }
+    }
+}
