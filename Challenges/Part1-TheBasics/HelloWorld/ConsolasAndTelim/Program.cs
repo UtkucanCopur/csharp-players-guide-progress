af69f9ab2e8e5bddf2e079366bf92ec5fcc9d34c
Utkucan Çopur <utkucan.copur00@gmail.com>
2026-01-10 21:50:13 +0300

    Hello world completed
    i completed all challenges in hello world session. Total 4 challenges done.

---
+++ Challenges/Part1-TheBasics/HelloWorld/ConsolasAndTelim/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolasAndTelim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bread is ready \nWho is the bread for?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Noted: {input} got bread");
+        }
+    }
+}
---
+++ Challenges/Part1-TheBasics/HelloWorld/TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMakingsOfAProgrammer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I");
+            Console.WriteLine("Think");
+            Console.WriteLine("Infinity");
+            Console.WriteLine("Statements");
+            Console.WriteLine("Can contain");
+        }
+    }
+}
