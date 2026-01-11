ea02e919bc387c50ddf9129d91d1a9a8ed70d6a1
Utkucan Çopur <utkucan.copur00@gmail.com>
2026-01-11 23:48:57 +0300

    decision making done
    i have completed two challenges in this session

---
+++ Challenges/Part1-TheBasics/DecisionMaking/RepairingTheClocktower/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepairingTheClocktower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give the number: ");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Tick");
+            } else
+            {
+                Console.WriteLine("Tock");
+            }
+        }
+    }
+}
---
+++ Challenges/Part1-TheBasics/DecisionMaking/Watchtower/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Watchtower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter x value: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter y value: ");
+            int y = int.Parse(Console.ReadLine());
+            if(x > 0)
+            {
+                if(y > 0)
+                {
+                    Console.WriteLine("The enemy is to the NE");
+                } else if (y < 0)
+                {
+                    Console.WriteLine("The enemy is to the SE");
+                } else
+                {
+                    Console.WriteLine("The enemy is to the E");
+                }
+            } else if (x < 0)
+            {
+                if (y > 0)
+                {
+                    Console.WriteLine("The enemy is to the NW");
+                }
+                else if (y < 0)
+                {
+                    Console.WriteLine("The enemy is to the SW");
+                }
+                else
+                {
+                    Console.WriteLine("The enemy is to the W");
+                }
+            } else
+            {
+                if (y > 0)
+                {
+                    Console.WriteLine("The enemy is to the N");
+                }
+                else if (y < 0)
+                {
+                    Console.WriteLine("The enemy is to the S");
+                }
+                else
+                {
+                    Console.WriteLine("The enemy is here!");
+                }
+            }
+            
+        }
+    }
+}
