73eb1f6533449202cf81a88acd64e425b456e478
Utkucan Çopur <utkucan.copur00@gmail.com>
2026-01-11 23:35:13 +0300

    Console 2.0 done
    i have completed one challenge in this session

---
+++ Challenges/Part1-TheBasics/Console2.0/TheDefenseOfConsolas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDefenseOfConsolas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter row: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter column: ");
+            int col = int.Parse(Console.ReadLine());
+            Console.WriteLine("Deploy to:");
+            Console.WriteLine($"{row},{col-1}");
+            Console.WriteLine($"{row},{col+1}");
+            Console.WriteLine($"{row-1},{col}");
+            Console.WriteLine($"{row+1},{col}");
+            Console.Title = "Defense of Consolas";
+            Console.Beep();
+        }
+    }
+}
