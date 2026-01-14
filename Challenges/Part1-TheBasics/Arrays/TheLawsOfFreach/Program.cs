8198932f311b7a96238b0daf107e1c5ced30f48a
Utkucan Çopur <utkucan.copur00@gmail.com>
2026-01-15 01:25:13 +0300

    Arrays session done
    i have completed two challenges in this session

---
+++ Challenges/Part1-TheBasics/Arrays/TheLawsOfFreach/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int currentSmallest = int.MaxValue;
+foreach(int index in array)
+{
+    if (index < currentSmallest)
+    {
+        currentSmallest = array[index];
+    }
+        
+}
+    
+    
+Console.WriteLine(currentSmallest);
+
+
+int total = 0;
+foreach (var index in array)
+{
+    total += index;
+}
+float average = (float)total / array.Length;
+Console.WriteLine(average);
---
+++ Challenges/Part1-TheBasics/Arrays/TheReplicatorOfD'To/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] arr1 = new int[5];
+for (int i = 0;  i < arr1.Length; i++)
+{
+    Console.WriteLine("Give number");
+    arr1[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int[] arr2 = new int[5];
+
+for (int i = 0;i < arr2.Length; i++)
+{
+    arr2[i] = arr1[i];
+}
+
+for (int i = 0; i < arr2.Length; i++)
+{
+    Console.WriteLine($"arr1: {arr1[i]}, arr2: {arr2[i]}");
+}
+
