5c71b230f83e9215738f55130eddf09515f10899
utkucan <utkucan.copur00@gmail.com>
2026-02-06 17:26:35 +0300

    Enum session is completed
    i have completed 1 challenge in this session.

---
+++ Challenges/Part2-ObjectOrientedProgramming/Enumerations/Simula's Test/Program.cs
@@ -0,0 +1,25 @@
+﻿Status chestStatus = Status.LOCKED;
+
+
+
+while (true)
+{
+    Console.WriteLine($"The chest is {chestStatus}. What do you want to do?");
+    string? choice = Console.ReadLine();
+    choice = choice?.ToLower();
+    if (choice == "unlock" && chestStatus == Status.LOCKED) chestStatus = Status.CLOSED;
+    else if (choice == "close" && chestStatus == Status.OPEN) chestStatus = Status.CLOSED;
+    else if (choice == "open" && chestStatus == Status.CLOSED) chestStatus = Status.OPEN;
+    else if (choice == "lock" && chestStatus == Status.CLOSED) chestStatus = Status.LOCKED;
+    else Console.WriteLine("Enter proper command");
+}
+
+
+enum Status
+{
+    OPEN,
+    CLOSED,
+    LOCKED
+}
+
+
