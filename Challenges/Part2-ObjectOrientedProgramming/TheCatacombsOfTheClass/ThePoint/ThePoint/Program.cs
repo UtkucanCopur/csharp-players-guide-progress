951eac9f08872c59b535616e467c25f96ff98e57
Utkucan Çopur <utkucan.copur00@gmail.com>
2026-06-24 11:52:32 +0300

    done The Point

---
+++ Challenges/Part2-ObjectOrientedProgramming/TheCatacombsOfTheClass/ThePoint/ThePoint/Program.cs
@@ -0,0 +1,30 @@
+﻿Point newPoint = new Point(2,3);
+Point anotherPoint = new Point(-4,0);
+
+Console.WriteLine($"newpoint= ({newPoint.X},{newPoint.Y})");
+Console.WriteLine($"newpoint= ({anotherPoint.X},{anotherPoint.Y})");
+
+
+
+
+public class Point
+{
+    public int X;
+    public int Y;
+
+    public Point()
+    {
+        X = 0;
+        Y = 0;
+    }
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    
+}
+
+// My properties are not immutable here because this is a simple program
+// so I didn't feel the need to use immutable properties.
